ff56fbbb096d29789f7cf59f0970664c7631cf03
simone-danzeri <s.danzeri@icloud.com>
2024-03-07 15:05:18 +0100

    human-code

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Generare i numeri da 1 a 100
+// Se il numero è multiplo di 3 e 5 --> stampo fizzbuzz
+// Se il numero è multiplo di 3 --> stampo fizz
+// Se il numero è multiplo di 5 --> stampo buzz
+// Se il numero non è multiplo di 3 o 5 --> stampo il numero
