9c2843d239196e5f12a2c3126a5fb983c188b4fb
simone-danzeri <s.danzeri@icloud.com>
2024-03-07 15:22:48 +0100

    feat: add fizz, buzz, fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 // Generare i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i)
+    // Se il numero è multiplo di 3 e 5 --> stampo fizzbuzz
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log('fizzbuzz');
+    // Se il numero è multiplo di 3 --> stampo fizz
+    } else if (i % 3 === 0){
+        console.log('fizz');
+    // Se il numero è multiplo di 5 --> stampo buzz
+    } else if (i % 5 === 0){
+        console.log('buzz');
+    // Se il numero non è multiplo di 3 o 5 --> stampo il numero
+    } else {
+        console.log(i);
+    }
 }
-// Se il numero è multiplo di 3 e 5 --> stampo fizzbuzz
-// Se il numero è multiplo di 3 --> stampo fizz
-// Se il numero è multiplo di 5 --> stampo buzz
-// Se il numero non è multiplo di 3 o 5 --> stampo il numero
+
+
