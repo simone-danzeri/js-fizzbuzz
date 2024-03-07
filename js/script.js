141679992cde53298020e2009ab41e07423ed00f
simone-danzeri <s.danzeri@icloud.com>
2024-03-07 15:07:14 +0100

    feat: add <for> cycle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 // Generare i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+}
 // Se il numero è multiplo di 3 e 5 --> stampo fizzbuzz
 // Se il numero è multiplo di 3 --> stampo fizz
 // Se il numero è multiplo di 5 --> stampo buzz
