db8e23ab9ec41d4b7bf46a3f6dce32c252a6e25f
simone-danzeri <s.danzeri@icloud.com>
2024-03-07 15:42:55 +0100

    feat: add bonus 1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
+let numbContainer = document.querySelector('#container');
 // Generare i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     // Se il numero è multiplo di 3 e 5 --> stampo fizzbuzz
     if ((i % 3 === 0) && (i % 5 === 0)) {
+        const newDiv = `<div>fizzbuzz</div>`;
+        numbContainer.innerHTML += newDiv;
         console.log('fizzbuzz');
     // Se il numero è multiplo di 3 --> stampo fizz
     } else if (i % 3 === 0){
+        newDiv = `<div>fizz</div>`;
+        numbContainer.innerHTML += newDiv;
         console.log('fizz');
     // Se il numero è multiplo di 5 --> stampo buzz
     } else if (i % 5 === 0){
+        newDiv = `<div>buzz</div>`;
+        numbContainer.innerHTML += newDiv;
         console.log('buzz');
     // Se il numero non è multiplo di 3 o 5 --> stampo il numero
     } else {
+        newDiv = `<div>${i}</div>`;
+        numbContainer.innerHTML += newDiv;
         console.log(i);
     }
 }
-
 
