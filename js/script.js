fe5e9d516c94e69962a24aa007572bf6f800a508
simone-danzeri <s.danzeri@icloud.com>
2024-03-08 10:47:07 +0100

    feat: add some colours

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ let numbContainer = document.querySelector('#container');
 for (let i = 1; i <= 100; i++) {
     // Se il numero è multiplo di 3 e 5 --> stampo fizzbuzz
     if ((i % 3 === 0) && (i % 5 === 0)) {
-        const newDiv = `<div>fizzbuzz</div>`;
+        const newDiv = `<div class='fizzbuzz'>fizzbuzz</div>`;
         numbContainer.innerHTML += newDiv;
         console.log('fizzbuzz');
     // Se il numero è multiplo di 3 --> stampo fizz
     } else if (i % 3 === 0){
-        newDiv = `<div>fizz</div>`;
+        newDiv = `<div class='fizz'>fizz</div>`;
         numbContainer.innerHTML += newDiv;
         console.log('fizz');
     // Se il numero è multiplo di 5 --> stampo buzz
     } else if (i % 5 === 0){
-        newDiv = `<div>buzz</div>`;
+        newDiv = `<div class='buzz'>buzz</div>`;
         numbContainer.innerHTML += newDiv;
         console.log('buzz');
     // Se il numero non è multiplo di 3 o 5 --> stampo il numero
